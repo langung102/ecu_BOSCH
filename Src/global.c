eb0227bdba33f9eeae9fd3d0a99be969eb63c7f4
minhlang102 <minhlangstudy@gmail.com>
2023-08-19 14:09:23 +0700

    dev

---
+++ Src/global.c
@@ -0,0 +1,17 @@
+/*
+ * global.c
+ *
+ *  Created on: Aug 19, 2023
+ *      Author: minhl
+ */
+
+#include "global.h"
+
+uint8_t temp = 0;
+uint8_t index_buffer = 0;
+uint8_t buffer_flag = 0;
+uint8_t str[20] = "GOATMESSI";
+
+uint8_t data_flag1  = 0;
+uint8_t data_flag2  = 0;
+uint8_t dataCounter = 0;
